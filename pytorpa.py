3342e7f1d43c4a7d7808afb4a227bb5f7a0cbddd
Praveekorn Chianglong <mtl91475@murata.com>
2023-11-06 15:37:39 +0700

    Auto check -v WebDriver

--- pytorpa.py
+++ pytorpa.py
@@ -1,33 +1,28 @@
-from selenium.webdriver.support.select import Select
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import time
 import pandas as pd
-from bs4 import BeautifulSoup as soup
-import openpyxl as OP
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup 
+import openpyxl as OP
 
-driver = webdriver.Chrome()
-
-driver.get("http://mtl91475:Gangkia@6@163.50.57.101/FC005/S000103.aspx")
-
+# driver = webdriver.Edge(r"C:\Users\mtl91475\Desktop\Coding\pySelenium\msedgedriver.exe")
+driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+driver.get("http://mtl91475:Gangkia@11@163.50.57.101/FC005/S000103.aspx")
 
 select = Select(driver.find_element(By.ID, 'MainContent_lbPTM0014')) # Process
 select2 = Select(driver.find_element(By.ID, 'MainContent_lbPTC0006')) # Input Code
-
 
 select.deselect_by_value("All")
 select2.deselect_by_value("All")
 
 
 # select.select_by_value("2191") # SMT
-select.select_by_value("2180") # Outgoing Inspection      
+select.select_by_value("2180") # Outgoing Inspection
 select2.select_by_value("12") # Fraction Combine
-time.sleep(3)
 
 driver.find_element(By.ID, 'MainContent_btnRefresh').click()
-time.sleep(3)
 
 ##------------------------------------------------------------------------------------------------------------##
 ############################################# Get from Wip ####################################################
@@ -35,7 +30,6 @@ time.sleep(3)
 data = driver.page_source
 dthtml = pd.read_html(data)[3]
 df = pd.DataFrame(dthtml)
-time.sleep(5)
 
 ##------------------------------------------------------------------------------------------------------------##
 ############################################# Get from Fraction and write to excel ####################################################
@@ -47,7 +41,7 @@ dff.to_csv("LotWip.csv")
 i=0
 while i < dfl:
     lotno = df[1][i+1]
-    driver.get("http://mtl91475:Gangkia@6@163.50.57.101/FC008/FractionCombineMasterLotHist.aspx")
+    driver.get("http://mtl91475:Gangkia@11@163.50.57.101/FC008/FractionCombineMasterLotHist.aspx")
     driver.find_element(By.ID, 'MainContent_txtNewLot').send_keys(lotno)
     driver.find_element(By.NAME, 'ctl00$MainContent$btnRefresh').click()
     soup = BeautifulSoup(driver.page_source)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-delete_rows
+
+dff.to_csv("LotWip.csv")
